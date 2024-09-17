6440b9372e5abfb991aef564e6a48c037309115f
Sevinch <sevinchsayfiddinova0512@gmail.com>
2024-09-16 23:56:57 -0400

    firts commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
+const productRoute = require("./routes/product.route.js");
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+//routes
+app.use("/api/products", productRoute);
+
+
+
+app.get('/', (req, res) => {
+    res.send("Hello fron Node API Server Updated");
+});
+
+
+
+
+
+
+
+
+mongoose
+.connect(
+    "mongodb+srv://sevinchsayfiddinova0512:sevinch0512@backenddb.pizux.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+)
+.then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000");
+    });
+})
+.catch(() => {
+    console.log("Connection failed!");
+});
